9647ea4dfca08610c5c1df5a41f13f9eff97ae9c
Adrien Navratil <adrien1975@live.fr>
2020-10-16 18:15:15 +0200

    Fixed MCQs date parsing

--- src/data/qcm.rs
+++ src/data/qcm.rs
@@ -58,10 +58,10 @@ pub async fn fetch_qcms(db: &DatabaseConnection, user: &User) -> DataResult<Vec<
     info!("Got {} mails", mails.len());
 
     let mut qcms: HashMap<String, QCMResult> = HashMap::new();
-
     for mail in mails.iter() {
-        let qcm_date = &mail.subject[28..38];
-        let naive_date = NaiveDate::parse_from_str(qcm_date, "%d/%m/%Y")
+        let to = mail.subject.len() - 14;
+        let qcm_date = String::from(&mail.subject[to - 6..to]) + "/2020";
+        let naive_date = NaiveDate::parse_from_str(&qcm_date, "%d/%m/%Y")
             .map_err(|e| DataError::DateParsingError {
                 date: qcm_date.to_owned(),
                 error: e
